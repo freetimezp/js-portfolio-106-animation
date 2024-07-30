dc2d72f1d5d3e47020783028e54e82508ac12100
freetimezp <kraftukrnet@ukr.net>
2024-07-31 00:35:53 +0300

    03. js-portfolio-106-animation. Animation GSAP.

---
+++ assets/js/data.js
@@ -0,0 +1,74 @@
+export const stories = [
+    {
+        profileImg: "./assets/images/profile-1.jpg",
+        profileName: "Behance",
+        title: [
+            "Showcasing creative",
+            "portfolios and projects",
+            "from top designers"
+        ],
+        linkLabel: "Read More",
+        linkSrc: "behance.net",
+        storyImg: "./assets/images/story-1.jpg"
+    },
+    {
+        profileImg: "./assets/images/profile-2.jpg",
+        profileName: "Dribbble",
+        title: [
+            "Inspiring design",
+            "ideas and visual",
+            "creations from experts"
+        ],
+        linkLabel: "Discover",
+        linkSrc: "dribbble.net",
+        storyImg: "./assets/images/story-2.jpg"
+    },
+    {
+        profileImg: "./assets/images/profile-3.jpg",
+        profileName: "Awwwards",
+        title: [
+            "Award-winning web",
+            "design and development",
+            "projects"
+        ],
+        linkLabel: "Check It Out",
+        linkSrc: "awwwards.net",
+        storyImg: "./assets/images/story-3.jpg"
+    },
+    {
+        profileImg: "./assets/images/profile-4.jpg",
+        profileName: "Adobe",
+        title: [
+            "Curated design",
+            "inspiration for",
+            "creative professionals"
+        ],
+        linkLabel: "Adobe More",
+        linkSrc: "adobe.net",
+        storyImg: "./assets/images/story-4.jpg"
+    },
+    {
+        profileImg: "./assets/images/profile-5.jpg",
+        profileName: "Creatibe Blog",
+        title: [
+            "The latest in",
+            "design trends",
+            "and tutorials"
+        ],
+        linkLabel: "Explore More",
+        linkSrc: "creativeblog.net",
+        storyImg: "./assets/images/story-5.jpg"
+    },
+    {
+        profileImg: "./assets/images/profile-6.jpg",
+        profileName: "Smashing Magazine",
+        title: [
+            "Practical tips",
+            "for web designers",
+            "and developers"
+        ],
+        linkLabel: "Visit Site",
+        linkSrc: "smashmagazine.net",
+        storyImg: "./assets/images/story-6.jpg"
+    },
+];
